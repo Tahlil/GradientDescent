ec01d891ce0e71540d17bb089d0c09f641e883a4
Tahlil1 <bsse0803@iit.du.ac.bd>
2019-08-21 22:47:46 +0600

    Refactoring

--- ML/gradientDescent.js
+++ ML/gradientDescent.js
@@ -1,51 +1,98 @@
-const { Matrix } = require('ml-matrix');
+const nj = require('numjs');
 
-let startingBeta1AndBeta2Approaches = {
-  randomApproach: (_, __) => {
+const GradientDescent = function(){
+  function _getRandomInARange(min, max) {
+    return Math.ceil(Math.random() * (max - min) + min);
+  }
 
-  },
-  roughEstimateApproach: (XYpairs) => {
-    let medians = XYpairs.reduce((previous, current) => {
-      
-    }, [{sumX: 0, sumY: 0}]);
-    let medianX = X.sort()[Math.floor(X.length/2)];
-    let medianY = Y.sort()[Math.floor(Y.length/2)];
+  let _startingConstantAndSlopeApproaches = {
+    randomApproach: (_) => {
+    },
+    roughEstimateApproach: (XYpairs) => {
+      console.log("Using random approach");
+      let X = XYpairs.map(point => parseInt(Object.keys(point)[0])),
+        Y = XYpairs.map(point => Object.values(point)[0]);
+      let numberOfObservation = X.length;
+      console.log(numberOfObservation);
+      let onesVector = nj.ones([numberOfObservation, 1]);
+      //let v =nj.array([new Array(numberOfObservation).fill(6)]);
+      //console.log(v); 
+
+      let XVector = nj.array(X).reshape(numberOfObservation, 1);
+      let XMatrix = nj.concatenate(onesVector, XVector);
+      // console.log(XVector);
+      console.log(XMatrix);
+      let medianX = X.sort()[Math.floor(numberOfObservation / 2)];
+      console.log("Median X: " + medianX);
+      let medianY = Y.sort()[Math.floor(numberOfObservation / 2)];
+      console.log("Median Y: " + medianY);
+
+      let minX = Math.min(...X);
+      console.log("Min Y: " + minX);
+
+      let estimatedConstant = XYpairs.filter(item => Object.keys(item)[0] == minX)[0];
+      //console.log(estimatedConstant);
+      estimatedConstant = Object.values(estimatedConstant)[0];
+      let estimatedYIntercept = (medianY - estimatedConstant) / medianX;
+      console.log("Estimated constant: " + estimatedConstant + "estimated YIntercept" + estimatedYIntercept);
+
+      let betaVector = nj.array([
+        [estimatedConstant],
+        [estimatedYIntercept]
+      ]);
+      console.log("Beta vector: ");
+      console.log(betaVector);
+      let yVector = nj.dot(XMatrix, betaVector);
+      console.log("yVector");
+      console.log(yVector);
+
+      return {
+        c: estimatedConstant,
+        m: estimatedYIntercept
+      };
+    }
+  }
+
+
+  _calculateCostFunction = () => {
 
   }
-}
 
-function _getRandomInARange(min, max) {
-  return Math.random() * (max - min) + min;
-}
+  runGradientDescent = (X, Y, learning_rate) => {
 
-_calculateCostFunction = () => {
-
-}
-
-runGradientDescent = (X, Y, learning_rate) => {
-
-}
-
-const createAndGetDummyData = (numberOfObservation, upperRange) => {
-  let data = [];
-  let yInterceptConstant = _getRandomInARange(100, 200), slope = _getRandomInARange(1, 10);
-  for (let index = 0; index < numberOfObservation; index++) {
-    let noise;
-    if(_getRandomInARange(0, 100)> 10){
-      noise = _getRandomInARange(-50, 50); 
-    }
-    else{
-      noise = _getRandomInARange(-500, 500); // outlier
-    }
-    let x = _getRandomInARange(20, upperRange);
-    let y = yInterceptConstant + x*slope +noise;
-    data.push({[x]: y});
   }
-  console.log(data);
-  return data;
-}
 
-module.exports = {
-  createAndGetDummyData: createAndGetDummyData,
-  runGradientDescent: runGradientDescent
-}
+  const createAndGetDummyData = (numberOfObservation, upperRange) => {
+    let data = [];
+    let yInterceptConstant = _getRandomInARange(100, 200),
+      slope = _getRandomInARange(1, 10);
+    console.log("Actual Beta0: " + yInterceptConstant + " Actual beta1: " + slope);
+
+    for (let index = 0; index < numberOfObservation; index++) {
+      let noise;
+      if (_getRandomInARange(0, 100) > 10) {
+        noise = _getRandomInARange(-50, 50);
+      } else {
+        noise = _getRandomInARange(-500, 500); // outlier
+      }
+      let x = _getRandomInARange(20, upperRange);
+      let y = yInterceptConstant + x * slope + noise;
+      data.push({
+        [x]: y
+      });
+    }
+    _startingConstantAndSlopeApproaches["roughEstimateApproach"](data);
+    return {
+      data: data,
+      beta0: yInterceptConstant,
+      beta1: slope
+    };
+  }
+
+  return {
+    createAndGetDummyData: createAndGetDummyData,
+    runGradientDescent: runGradientDescent
+  }
+}();
+
+module.exports = GradientDescent;
--- main.js
+++ main.js
@@ -4,52 +4,76 @@ const gradientDescent = require('./ML/gradientDescent');
 
 let win;
 
-function createWindow () {
-  win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
+class DesktopApp {
+
+  constructor() {
+    this.createWindow();
+  }
+  
+  createWindow () {
+      win = new BrowserWindow({
+        width: 1600,
+        height: 900,
+        webPreferences: {
+          nodeIntegration: true
+        }
+      })
+      globalShortcut.register('f5', function() {
+        console.log('f5 is pressed')
+        win.reload()
+      })
+      globalShortcut.register('CommandOrControl+R', function() {
+        console.log('CommandOrControl+R is pressed')
+        win.reload()
+      })
+      win.loadFile('index.html')
+    
+      win.webContents.openDevTools()
+    
+      win.on('closed', () => {
+        win = null
+      })
     }
-  })
-  globalShortcut.register('f5', function() {
-		console.log('f5 is pressed')
-		win.reload()
-	})
-	globalShortcut.register('CommandOrControl+R', function() {
-		console.log('CommandOrControl+R is pressed')
-		win.reload()
-	})
-  win.loadFile('index.html')
-
-  win.webContents.openDevTools()
-
-  win.on('closed', () => {
-    win = null
-  })
+    
+    app.on('ready', createWindow)
+    
+    app.on('window-all-closed', () => {
+      if (process.platform !== 'darwin') {
+        app.quit()
+      }
+    })
+    
+    app.on('activate', () => {
+      if (win === null) {
+        createWindow()
+      }
+    })
+    
+    ipcMain.on('get-data', (event, arg) => {
+      let allInfos = gradientDescent.createAndGetDummyData(10, 1000);
+      let data = allInfos.data;
+      data = data.map((point)=> {
+        let key = Object.keys(point)[0];
+        let value = point[key];
+        return {regressor: parseInt(key), regressand: value};
+      });
+      //console.log(arg) // prints "ping"
+      event.sender.send('asynchronous-reply', data);
+    })
+    
+    
+    ipcMain.on('run-GD-algo', (event, arg) => {
+      let allInfos = gradientDescent.createAndGetDummyData(10, 1000);
+      let data = allInfos.data;
+      data = data.map((point)=> {
+        let key = Object.keys(point)[0];
+        let value = point[key];
+        return {regressor: parseInt(key), regressand: value};
+      });
+      //console.log(arg) // prints "ping"
+      event.sender.send('asynchronous-reply', data);
+    })
+  
 }
 
-app.on('ready', createWindow)
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
-app.on('activate', () => {
-  if (win === null) {
-    createWindow()
-  }
-})
-
-ipcMain.on('get-data', (event, arg) => {
-  let data = gradientDescent.createAndGetDummyData(1000, 1000);
-  data = data.map((point)=> {
-    let key = Object.keys(point)[0];
-    let value = point[key];
-    return {regressor: parseInt(key), regressand: value};
-  });
-  //console.log(arg) // prints "ping"
-  event.reply('asynchronous-reply', data)
-})
+module.exports = DesktopApp;
